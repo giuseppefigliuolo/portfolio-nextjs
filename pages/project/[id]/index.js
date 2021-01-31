54d3aff46b80ab9637ee9afa23d9f041e8dd2b02
giuseppefigliuolo <figliuologiuseppe@gmail.com>
2021-01-31 18:50:57 +0100

    api header set

--- pages/project/[id]/index.js
+++ pages/project/[id]/index.js
@@ -121,7 +121,15 @@ const project = ({ project }) => {
 };
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(`${server}/api/projects/${context.params.id}`);
+  const res = await fetch(`${server}/api/projects/${context.params.id}`, {
+    method: "GET",
+    headers: {
+      // update with your user-agent
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+      Accept: "application/json; charset=UTF-8",
+    },
+  });
 
   const project = await res.json();
 
