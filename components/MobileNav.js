51209dbbd474d58b72dd3d472e8e4143fc65f6c5
giuseppefigliuolo <figliuologiuseppe@gmail.com>
2021-02-03 15:46:57 +0100

    mobile menu fixed

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -2,16 +2,29 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import logoSvg from "../public/img/logo-white-fg.svg";
 import menuSvg from "../public/img/hamb-menu.svg";
-import SocialLogo from "./SocialLogo/SocialLogo";
-import closeMenu from "../public/img/close-menu.svg";
-import githubLogo from "../public/img/logos/social/orange-github.svg";
-import linkedInLogo from "../public/img/logos/social/orange-linkedin.svg";
-import instagramLogo from "../public/img/logos/social/orange-instagram.svg";
 import { server } from "../config/index";
-import { motion } from "framer-motion";
-import { debounce } from "../utils/debounce";
+import { motion, AnimatePresence } from "framer-motion";
+import { transition } from "../utils/fm-transitions";
+
+const menuVariants = {
+  hidden: {
+    y: -300,
+  },
+  visible: {
+    y: 0,
+    transition: { ...transition },
+  },
+  exit: {
+    y: -300,
+    transition: { ...transition },
+  },
+};
 
 const MobileNav = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const closeMenu = () => setShowMenu(false);
+
   return (
     <div>
       <nav className="mobile">
@@ -23,50 +36,38 @@ const MobileNav = () => {
               </a>
             </Link>
           </div>
-          <div className="hamb-menu">
+          <div className="hamb-menu" onClick={() => setShowMenu(!showMenu)}>
             <img src={menuSvg} />
           </div>
         </div>
-        <div className="inner-menu">
-          <div className="close-menu">
-            <img src={closeMenu} />
-          </div>
-          <div className="logo">
-            <img src={logoSvg} />
-          </div>
-          <div className="inner-menu__links">
-            <div className="container">
-              <Link href={`/#about`}>
-                <a>About</a>
-              </Link>
-              <Link href={`/#works`}>
-                <a>Works</a>
-              </Link>
-              <Link href={`/#skills`}>
-                <a>Skills</a>
-              </Link>
-              <Link href={`/#contact`}>
-                <a>Contact Me</a>
-              </Link>
-            </div>
-          </div>
-          <div className="inner-menu__social">
-            <div className="container">
-              <SocialLogo
-                link="https://github.com/giuseppefigliuolo"
-                img={githubLogo}
-              />
-              <SocialLogo
-                link="https://www.linkedin.com/in/giuseppe-figliuolo-51103818a/"
-                img={linkedInLogo}
-              />
-              <SocialLogo
-                link="https://www.instagram.com/giu_figliuolo/"
-                img={instagramLogo}
-              />
-            </div>
-          </div>
-        </div>
+        <AnimatePresence>
+          {showMenu && (
+            <motion.div
+              className="inner-menu"
+              variants={menuVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              <div className="inner-menu__links">
+                <div className="container">
+                  <Link href={`${server}/#about`}>
+                    <a onClick={closeMenu}>About</a>
+                  </Link>
+                  <Link href={`${server}/#works`}>
+                    <a onClick={closeMenu}>Works</a>
+                  </Link>
+                  <Link href={`${server}/#skills`}>
+                    <a onClick={closeMenu}>Skills</a>
+                  </Link>
+                  <Link href={`${server}/#contact`}>
+                    <a onClick={closeMenu}>Contact Me</a>
+                  </Link>
+                </div>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </nav>
     </div>
   );
