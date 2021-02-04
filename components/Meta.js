49d6965de31fa18b7bac0eb82f1308644c47421d
giuseppefigliuolo <figliuologiuseppe@gmail.com>
2021-02-04 18:25:04 +0100

    favicon added and overflow fixed for mobile skills' section

--- components/Meta.js
+++ components/Meta.js
@@ -15,7 +15,7 @@ const Meta = ({ title, keywords, description }) => {
         href="https://fonts.googleapis.com/css2?family=Changa:wght@200;300;400;500&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap"
         rel="stylesheet"
       />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
       <title>{title}</title>
     </Head>
